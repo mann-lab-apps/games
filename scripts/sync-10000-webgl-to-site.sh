ada72ea4357f998a2b9ed5e2c2751576eb9a50fc
jaeman Kim <daga4242@gmail.com>
2026-07-29 11:14:31 +0900

    Bust Unity WebGL asset cache

--- scripts/sync-10000-webgl-to-site.sh
+++ scripts/sync-10000-webgl-to-site.sh
@@ -20,6 +20,9 @@ gzip -dc "$target_dir/Build/10000.wasm.gz" > "$target_dir/Build/10000.wasm"
 
 perl -pi -e 's/10000\.data\.gz/10000.data/g; s/10000\.framework\.js\.gz/10000.framework.js/g; s/10000\.wasm\.gz/10000.wasm/g' "$target_dir/index.html"
 perl -pi -e 's/canvas\.style\.width = "960px";/canvas.style.width = "100%";/g; s/canvas\.style\.height = "600px";/canvas.style.height = "100%";/g' "$target_dir/index.html"
+asset_version="$(shasum -a 256 "$target_dir/Build/10000.wasm" | awk '{ print substr($1, 1, 12) }')"
+perl -0pi -e "s/var buildUrl = \"Build\";\\n      var loaderUrl = buildUrl \\+ \"\\/10000\\.loader\\.js\";/var buildUrl = \"Build\";\\n      var assetVersion = \"$asset_version\";\\n      var versionSuffix = \"?v=\" + assetVersion;\\n      var loaderUrl = buildUrl + \"\\/10000.loader.js\" + versionSuffix;/" "$target_dir/index.html"
+perl -pi -e 's/(dataUrl: buildUrl \+ "\/10000\.data")/$1 + versionSuffix/; s/(frameworkUrl: buildUrl \+ "\/10000\.framework\.js")/$1 + versionSuffix/; s/(codeUrl: buildUrl \+ "\/10000\.wasm")/$1 + versionSuffix/' "$target_dir/index.html"
 
 rm "$target_dir/Build/10000.data.gz" \
   "$target_dir/Build/10000.framework.js.gz" \
